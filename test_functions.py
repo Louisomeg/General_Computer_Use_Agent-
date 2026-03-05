7765cdfbd7e9a40f34492f2af5386f7469f57ccc
jorge-mells <jorgemelloes@gmail.com>
2026-03-05 22:44:41 +0100

    improve: try out new computer use functions again

--- test_functions.py
+++ test_functions.py
@@ -5,22 +5,27 @@ from core.desktop_executor import DesktopExecutor
 
 test_client = Client()
 de_exec = DesktopExecutor()
-function_names = [
-    de_exec._click_at,
-    de_exec._hover_at,
-    de_exec._type_text_at,
-    de_exec._scroll_document,
-    de_exec._scroll_at,
-    de_exec._wait,
-    de_exec._key_combination,
-    de_exec._drag_and_drop,
+excluded_functions = [
+    "click_at",
+    "hover_at",
+    "type_text_at",
+    "scroll_document",
+    "scroll_at",
+    "wait_5_seconds",
+    "key_combination",
+    "drag_and_drop",
 ]
-functions = [
-    types.FunctionDeclaration.from_callable(client=test_client, callable=c)
-    for c in function_names
-]
-config = {"tools": [types.Tool(function_declarations=functions)]}
-test_loop = AgenticLoop(test_client)
+config = {
+    "tools": [
+        types.Tool(
+            computer_use=types.ComputerUse(
+                environment=types.Environment.ENVIRONMENT_BROWSER,
+                excluded_predefined_functions=excluded_functions,
+            )
+        )
+    ]
+}
+test_loop = AgenticLoop(test_client, config=config)
 test_loop.agentic_loop(
     "print all the functions you have. then open freecad with these functions", de_exec
 )
